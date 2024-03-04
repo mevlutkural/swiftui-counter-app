9ab38fc01f0ebc792c2c89880bc0b6315270cf8d
sam haze <mevlutkural4270@gmail.com>
2024-03-04 22:55:03 +0300

    Initial commit

--- swiftui-counter-app/ContentView.swift
+++ swiftui-counter-app/ContentView.swift
@@ -1,19 +1,32 @@
 //
 //  ContentView.swift
-//  swiftui-counter-app
+//  learn_swift_1
 //
-//  Created by sam on 4.03.2024.
+//  Created by sam on 3.03.2024.
 //
 
 import SwiftUI
 
+struct Counter: View {
+    @State private var counter = 0;
+    
+    var body: some View {
+        HStack {
+            Button("-") {
+                counter -= 1;
+            }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Text("\(counter)").font(.largeTitle)
+            Button("+") {
+                counter += 1;
+            }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Counter()
         }
         .padding()
     }
